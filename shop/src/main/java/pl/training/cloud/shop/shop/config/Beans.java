fa2c9bec7fe6dbc0d8fbe3c288e7e98b32aa178e
makar1811 <makar1811>
2019-10-22 11:29:05 +0200

    dodanie modułu shop

---
+++ shop/src/main/java/pl/training/cloud/shop/shop/config/Beans.java
@@ -0,0 +1,28 @@
+package pl.training.cloud.shop.shop.config;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+@EnableBinding(Sink.class)
+@EnableSwagger2
+@Configuration
+public class Beans {
+
+    private static final String BASE_PACKAGE = "pl.training.cloud.shop";
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(SWAGGER_2)
+                .select()
+                .apis(basePackage(BASE_PACKAGE))
+                .build();
+    }
+
+}
